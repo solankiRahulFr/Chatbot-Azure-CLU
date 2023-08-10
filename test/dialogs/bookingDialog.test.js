00e9c1ed094b08f5cf6bac7e80be576e798a8a3f
solankiRahulFr <99829859+solankiRahulFr@users.noreply.github.com>
2023-08-10 17:39:51 +0200

    Update bookingDialog.test.js

--- test/dialogs/bookingDialog.test.js
+++ test/dialogs/bookingDialog.test.js
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-
 /* eslint-env node, mocha */
 const { DialogTestClient, DialogTestLogger } = require('botbuilder-testing');
 const { BookingDialog } = require('../../dialogs/bookingDialog');
